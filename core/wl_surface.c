324c12f7c8f39ac0b20948143617d4a67e79ccbc
Stefano Ragni <st3r4g@protonmail.com>
2019-08-21 11:01:42 +0200

    surface: use the most recent frame resource

--- core/wl_surface.c
+++ core/wl_surface.c
@@ -36,8 +36,10 @@ static void damage(struct wl_client *client, struct wl_resource
 static void frame(struct wl_client *client, struct wl_resource
 *resource, uint32_t callback) {
 	struct surface *surface = wl_resource_get_user_data(resource);
-	if (!surface->frame)
-		surface->frame = wl_resource_create(client, &wl_callback_interface, 1, callback);
+	if (surface->frame)
+		wl_resource_destroy(surface->frame);
+
+	surface->frame = wl_resource_create(client, &wl_callback_interface, 1, callback);
 }
 
 static void set_opaque_region(struct wl_client *client, struct
