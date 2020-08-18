1474f59eae08201fca4b96fd164aa985256e7b80
Ella-0 <23418164+Ella-0@users.noreply.github.com>
2020-08-18 20:42:11 +0000

    Fix clang compilation whith format string

--- src/main.c
+++ src/main.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[]) {
 		case 'k': kdevpath = optarg; break;
 		case 'p': pdevpath = optarg; break;
 		default:
-			printf(usage);
+			printf("%s", usage);
 			return EXIT_FAILURE;
 		}
 	}
